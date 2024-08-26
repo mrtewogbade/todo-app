83b4cea3b9bff713437fbc83ce682d54ce5fc820
mrtewogbade <johndharey@gmail.com>
2024-08-26 11:58:03 +0100

    updated

--- serviceUrl.ts
+++ serviceUrl.ts
@@ -1,9 +1,4 @@
 //This is where we will have all our base Urls
 import dotenv from "dotenv";
-dotenv.config();
-export const Termii_BASE_URL = process.env.TERMII_BASE_URL;
 export const PORT = process.env.PORT;
-export const COOKIE_SECRET = process.env.COOKIE_SECRET;
-export const AWS_REGION = process.env.AWS_REGION;
-
 export const DB_URI = process.env.DB_URI;
